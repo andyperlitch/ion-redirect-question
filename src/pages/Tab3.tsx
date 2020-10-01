c7e13c738aab0a113bc446e61f8222bc62076305
Andy Perlitch <aperlitch@netflix.com>
2020-10-01 11:00:03 -0700

    failing redirect

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { Redirect, useParams } from 'react-router'
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
+  const { id } = useParams<{
+    id: string
+  }>()
+
+  // If no :id param in path, redirect
+  if (!id) {
+    return <Redirect to="/tab3/1" />
+  } else {
+    console.log(`tab3: id present (${id})`)
+  }
   return (
     <IonPage>
       <IonHeader>
